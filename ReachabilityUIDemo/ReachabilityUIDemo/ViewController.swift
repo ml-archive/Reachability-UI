8ce04324dfc58cc46ebfbb9c241dcacd001cec2b
anho <anho@nodes.dk>
2018-10-03 13:37:38 +0200

    added Xcode project

---
+++ ReachabilityUIDemo/ReachabilityUIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReachabilityUIDemo
+//
+//  Created by Andrei Hogea on 03/10/2018.
+//  Copyright © 2018 Nodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
