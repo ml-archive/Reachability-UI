3be6eead15c7c4ece53c13b11c243d2e20335ed6
anho <anho@nodes.dk>
2018-10-04 16:48:00 +0200

    cleaned project

---
+++ ReachabilityUIDemo/Carthage/Checkouts/Reachability-UI/ReachabilityUIDemo/ReachabilityUIDemo/Scenes/TabBar/TabBarModels.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarModels.swift
+//  ReachabilityUIDemo
+//
+//  Created by Andrei Hogea on 03/10/2018.
+//  Copyright © 2018 Nodes. All rights reserved.
+//
+
+import Foundation
+
+struct TabBar {
+    enum Scene {
+        case hello
+        case world
+    }
+}
